ecab263f83469df078d4b0db11d629176f5be223
Anubhab Mukherjee <anubhab.mukherjee2013@gmail.com>
2019-06-16 21:56:47 +0530

    Press enter after typing password to submit it.

--- main.cpp
+++ main.cpp
@@ -12,16 +12,27 @@ int main(int argc, char *argv[])
 
     QDialog widget;
     QVBoxLayout layout(&widget);
+    QHBoxLayout passField(&widget);
     QLabel label("Enter your password\nBut remember, this app is not secure",&widget);
     QLineEdit input(&widget);
-    QPushButton button("Submit",&widget);
+    QPushButton button("Submit",&widget), showPass(&widget);
     input.setEchoMode(QLineEdit::EchoMode::Password);
+    showPass.setFlat(true);
     QObject::connect(&button,&QPushButton::clicked,&input,[&input](){
         std::cout << input.text().toStdString() << std::endl;
         exit(0);
     });
+    QObject::connect(&input,&QLineEdit::returnPressed,&button,&QPushButton::click);
+    QObject::connect(&showPass,&QPushButton::clicked,&input,[&input](){
+        if(input.echoMode() == QLineEdit::EchoMode::Password)
+            input.setEchoMode(QLineEdit::EchoMode::Normal);
+        else
+            input.setEchoMode(QLineEdit::EchoMode::Password);
+    });
     layout.addWidget(&label);
-    layout.addWidget(&input);
+    passField.addWidget(&input);
+    passField.addWidget(&showPass);
+    layout.addLayout(&passField);
     layout.addWidget(&button);
     widget.setLayout(&layout);
     widget.show();
